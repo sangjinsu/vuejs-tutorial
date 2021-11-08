eecbe7061d19d3255c479b9d88a6b94f0bef79b5
jinsuSang <climbplant39@kakao.com>
2021-11-08 13:18:10 +0900

    feat: create and register vue component

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
+  </div>
+</template>
+
+<script>
+import TodoHeader from '/components/TodoHeader.vue'
+import TodoInput from '/components/TodoInput.vue'
+import TodoList from '/components/TodoList.vue'
+import TodoFooter from '/components/TodoFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    input
+  </div>
+</template>
+
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
